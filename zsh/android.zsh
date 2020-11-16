b911ec103ba98fae2989bf5dec5852383d3fb5a5
TheCao <caominhthetg@gmail.com>
2020-11-16 19:28:47 +0700

    fix: username

--- zsh/android.zsh
+++ zsh/android.zsh
@@ -1,3 +1,3 @@
 export JAVA_HOME='/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home'
-export ANDROID_HOME=/Users/trongtran/Library/Android/sdk
-export ANDROID_SDK_ROOT=/Users/trongtran/Library/Android/sdk
+export ANDROID_HOME=/Users/minhthecao/Library/Android/sdk
+export ANDROID_SDK_ROOT=/Users/minhthecao/Library/Android/sdk
